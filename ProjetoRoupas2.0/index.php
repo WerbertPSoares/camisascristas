af9c9e91b2e60d7011290e2d1c5ef4dfc82161b2
WerbertPSoares <wp.soares0107@gmail.com>
2024-12-04 16:33:45 -0300

    s

---
+++ ProjetoRoupas2.0/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="favicon-32x32.png" type="image/x-icon">
+    <title>Camisas Cristãs - Loja Online</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!-- Cabeçalho -->
+    <header>
+        <div class="logo">
+            <h1>Loja Emaús</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="cart.html">Carrinho</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Seção principal -->
+    <main>
+        <section class="product-gallery">
+            <h2>Camisas Cristãs</h2>
+            <div class="product-list">
+                <!-- Produto 1 -->
+              
+                 <div class="product-card" data-id="1" data-name="Camisa Fé" data-price="69.90">
+                     <img src="https://img.elo7.com.br/product/zoom/22F857D/camiseta-camisa-fe-presente-amigo-secreto.jpg" alt="Camisa Cristã 1">
+                     <h3>Camisa Fé</h3>
+                     <p>Preço: R$ 69,90</p>
+                     <button class="add-to-cart">Adicionar ao Carrinho</button>
+                  </div>
+                <!-- Produto 2 -->
+                <div class="product-card" data-id="2" data-name="Camisa Esperança" data-price="69.90">
+                    <img src="https://img.elo7.com.br/product/zoom/2D63811/camiseta-hope-esperanca-camisa-crista-camisa-gospel.jpg" alt="Camisa Cristã 2">
+                    <h3>Camisa Esperança</h3>
+                    <p>Preço: R$ 69,90</p>
+                    <button class="add-to-cart">Adicionar ao Carrinho</button>
+                 </div>
+
+                <!-- Produto 3 -->
+                <div class="product-card" data-id="3" data-name="Camisa Amor" data-price="79.90">
+                    <img src="https://m.media-amazon.com/images/I/41DTLBlUYbL._AC_.jpg" alt="Camisa Cristã 3">
+                    <h3>Camisa Amor</h3>
+                    <p>Preço: R$ 79,90</p>
+                    <button class="add-to-cart">Adicionar ao Carrinho</button>
+                 </div>
+            </div>
+        </section>
+    </main>
+
+    <!-- Rodapé -->
+    <footer>
+        <p>&copy; 2024 Loja Cristã | Todos os direitos reservados</p>
+    </footer>
+
+    <script src="script.js"></script>
+    
+</body>
+</html>
